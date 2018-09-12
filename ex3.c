4547c88a95599599a90c71ff6e58f52beb4e4f2d
Danilum <35626917+Danilum@users.noreply.github.com>
2018-09-12 19:51:46 +0300

    ex3.c
    ex3.c

---
+++ ex3.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  task3
+//
+//  Created by Daniil Shilintsev on 12/09/2018.
+//  Copyright © 2018 Daniil Shilintsev. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char command[100];
+    while (1) {
+        printf("--> ");
+        scanf("%s", command);
+        system(command);
+    }
+}
